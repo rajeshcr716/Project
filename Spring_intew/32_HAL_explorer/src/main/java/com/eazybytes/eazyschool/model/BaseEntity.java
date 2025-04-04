23b4f6172dc41ca7977c6ac657451842597be874
Rajesh C R <rajesh.c-r@hpe.com>
2025-04-04 13:08:10 +0530

    32_HAL_explorer UPDATE

---
+++ Spring_intew/32_HAL_explorer/src/main/java/com/eazybytes/eazyschool/model/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.eazybytes.eazyschool.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @JsonIgnore
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    @JsonIgnore
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    @JsonIgnore
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    @JsonIgnore
+    private String updatedBy;
+}
